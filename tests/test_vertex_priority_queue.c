ac1e9d2f95624d795d4d64abf8c912c5b7deb6ea
kacper.wiaczek00@gmail.com <kacper.wiaczek00@gmail.com>
2022-03-17 22:17:40 +0100

    updated test

--- tests/test_vertex_priority_queue.c
+++ tests/test_vertex_priority_queue.c
@@ -16,23 +16,24 @@ QueuedVertex * test_vertex(int index, double dist)
 
 int main(int argc, char * argv[])
 {
-    VertexPriorityQueue * pr = vertex_priority_queue_initalize(10);
+    VertexPriorityQueue * pr = vertex_priority_queue_initalize(100);
 
-    for(int i = 9; i != -1; i--)
+    for(int i = 99; i != -1; i--)
     {
         vertex_priority_queue_add(pr, test_vertex(i, DBL_MAX));
     }
 
-    vertex_priority_queue_update(pr, 5, 13);
-    vertex_priority_queue_poll(pr);
-    vertex_priority_queue_update(pr, 1, 96);
-    vertex_priority_queue_update(pr, 2, 13);
+    for(int i = 99; i != -1; i--)
+    {
+        vertex_priority_queue_update(pr, i, 99-i);
+    }
+
 
   
     while(pr->size != 0)
     {
         QueuedVertex * p = vertex_priority_queue_poll(pr);
-        printf("%d - %g\n", p->index, p->dist);
+        printf("Vertex(index=%d, dist=%g)\n", p->index, p->dist);
 
         free(p);
     }
